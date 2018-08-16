cb6545229d9811e2c29862fd8ba6ff0ea1fab41f
gehlerb <33291073+gehlerb@users.noreply.github.com>
2018-08-17 01:23:18 +0300

    commit 6

--- RecSearch/RecSearch/app/src/main/java/com/example/baruch/recsearch/MainActivity.java
+++ RecSearch/RecSearch/app/src/main/java/com/example/baruch/recsearch/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.baruch.recsearch;
 
 import android.Manifest;
-import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<AudioFile> AudioList;
+    List<AudioFile> audioList;
     ListView AudioListView;
+    SearchResults results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         isStoragePermissionGranted();
     }
 
-    private void initAudioList() {
+     private void initAudioList(final String word) {
 
         new AsyncTask<Void, Void, List<AudioFile>>() {
             @Override
@@ -35,14 +39,14 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             protected List<AudioFile> doInBackground(Void... params) {
-                return AudioList; //DB_ManagerFactory.getDb_manager().getBranches();
+                return results.getResultList(word);
             }
 
             @Override
             protected void onPostExecute(List<AudioFile> Audios) {
-                AudioList = Audios;
+                audioList = Audios;
                 if (Audios != null) {
-                    //setAudiosListView();
+                    setAudiosListView();
                 }
                 else {
                     Toast.makeText(MainActivity.this, "there are no Audio to show", Toast.LENGTH_SHORT).show();
@@ -50,6 +54,25 @@ public class MainActivity extends AppCompatActivity {
             }
         }.execute();
     }
+
+    private void setAudiosListView() {
+        ArrayAdapter<AudioFile> adapter = new ArrayAdapter<AudioFile>(this, R.layout.audio_item_view, audioList) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = View.inflate(MainActivity.this, R.layout.audio_item_view, null);
+                }
+                TextView dateTextView = (TextView) convertView.findViewById(R.id.dateTextView);
+                TextView numberOfTimesTextView = (TextView) convertView.findViewById(R.id.numberOfTimestextView);
+                TextView fileNameTextView = (TextView) convertView.findViewById(R.id.fileNameTextView);
+
+
+                return convertView;
+            }
+        };
+        AudioListView.setAdapter(adapter);
+    }
+
     public boolean isStoragePermissionGranted() {
         if (Build.VERSION.SDK_INT >= 23) {
             if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
