a7b802fbf7d0a2999854debfe85f1a126f859cd3
gehlerb <33291073+gehlerb@users.noreply.github.com>
2018-08-16 23:14:15 +0300

    third commit

--- RecSearch/RecSearch/app/src/main/java/com/example/baruch/recsearch/AudioFile.java
+++ RecSearch/RecSearch/app/src/main/java/com/example/baruch/recsearch/AudioFile.java
@@ -1,6 +1,11 @@
 package com.example.baruch.recsearch;
 
+import java.util.Date;
+import java.util.List;
+
 public class AudioFile {
     public String name;
+    public List<TimeWord> result;
+    public Date date;
     
 }
---
+++ RecSearch/RecSearch/app/src/main/java/com/example/baruch/recsearch/TimeWord.java
@@ -0,0 +1,8 @@
+package com.example.baruch.recsearch;
+
+import java.sql.Time;
+
+public class TimeWord {
+    public String word;
+    public Time time;
+}
